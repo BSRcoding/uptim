6a0be4e8d1fda8ef0fb5e220016534d99c4097d0
BSRcoding <bhushan172007@gmail.com>
2024-11-28 14:36:11 +0530

    Update Dockerfile.py

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,8 +1,28 @@
-# Clone the repository
-RUN git clone https://github.com/BSRcoding/uptim.git /root/ptb
+# Use a slim Python image
+FROM python:3.8.5-slim-buster
 
-# Set the working directory
+# Set environment variables
+ENV PIP_NO_CACHE_DIR=1
+
+# Update and install necessary packages
+RUN apt-get update && apt-get install -y \
+    git \
+    curl \
+    bash \
+    libffi-dev \
+    libpq-dev \
+    python3-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Clone the repository containing the bot code
+RUN git clone https://github.com/BSRcoding/waifu.git /root/ptb
+
+# Set working directory to the cloned repo
 WORKDIR /root/ptb
 
-# Install requirements
+# Install the Python dependencies
+RUN pip install --upgrade pip setuptools
 RUN pip install -r requirements.txt
+
+# Set the command to start the bot using python3 -m shivu
+CMD ["python3", "-m", "shivu"]
